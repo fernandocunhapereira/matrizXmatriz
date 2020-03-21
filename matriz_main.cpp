d2e9dd6196a95e765db8e30d7f1d8eca62bad48a
fernandocunhapereira <fernandocunhapereira@gmail.com>
2020-03-21 15:22:47 -0300

    testes ok matriz 2x2/2x2 2x3/3x2

--- matriz_main.cpp
+++ matriz_main.cpp
@@ -2,18 +2,22 @@
 #include "matriz.h"
 
 int main(int argc, char* argv[]){
-  Matriz a(2,2);
-  Matriz b(2,2);
+  Matriz a(2,3);
+  Matriz b(3,2);
 
   a.Matriz::setValor(2,0,0);
   a.Matriz::setValor(3,1,0);
+  a.Matriz::setValor(4,0,2);
   a.Matriz::setValor(1,0,1);
   a.Matriz::setValor(1,1,1);
+  a.Matriz::setValor(5,1,2);
 
   b.Matriz::setValor(2,0,0);
   b.Matriz::setValor(1,1,0);
   b.Matriz::setValor(5,0,1);
   b.Matriz::setValor(1,1,1);
+  b.Matriz::setValor(3,2,1);
+  b.Matriz::setValor(4,2,0);
 
   std::cout<<"matriz a"<<std::endl;
   for(int i=0; i<a.getLinha();i++){
